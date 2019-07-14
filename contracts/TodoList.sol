e8b0b150db8f2bf29109f97e3cdf4027fb3f035a
Mukheem <mukheemuddin@gmail.com>
2019-07-14 19:11:46 +0530

    BC done for Todo List. Yet to prepare front end

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.5.7;
 
 contract TodoList{
-    uint taskCount = 0;
+    uint public taskCount = 0;
+    struct task{
+        uint id;
+        string description;
+        bool completed;
+    }
+    mapping(uint => task) public tasks;
+    constructor() public{
+        createTask("Bismillah");
+    }
+    function createTask(string memory _description) public{
+        taskCount++;
+        tasks[taskCount] = task(taskCount,_description,false);
+    }
 }
